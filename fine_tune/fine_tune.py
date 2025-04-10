57f89b048caaaf393ba92b45bee53e1640a6678f
yutian929 <yutian929@outlook.com>
2025-04-10 11:14:44 +0800

    failed. fine tuning.

--- fine_tune/fine_tune.py
+++ fine_tune/fine_tune.py
@@ -11,8 +11,9 @@ results[0].show()
 
 # Train the model on the COCO8 example dataset for 100 epochs
 yaml_path = os.path.join(os.path.dirname(__file__), "fine_tune.yaml")
-results = model.train(data=yaml_path, epochs=100, imgsz=640)
+results = model.train(data=yaml_path, epochs=10, imgsz=640)
 
 # second try, with fine tuning
 model.set_classes(["person", "horse"])
 results = model.predict("test.jpg")
+results[0].show()
